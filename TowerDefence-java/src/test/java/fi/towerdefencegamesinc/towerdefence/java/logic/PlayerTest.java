c4d889dc947417dcace7f5863418bd5f25925457
Ville-Veikko Saari <ville-veikko.saari@cs.helsinki.fi>
2017-05-19 13:22:19 +0300

    Initial project created with maven and pit-testing

---
+++ TowerDefence-java/src/test/java/fi/towerdefencegamesinc/towerdefence/java/logic/PlayerTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.towerdefencegamesinc.towerdefence.java.logic;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author vrsaari
+ */
+public class PlayerTest {
+    
+    public PlayerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void playerIsCreated() {
+        Player player = new Player("Uolevi");
+        assertEquals("Uolevi", player.getName());
+    }
+}
